29289f265e17d496827e45a2ff3968ba94612c89
Tuan Nguyen <tuan.nat915@gmail.com>
2023-10-03 19:40:45 -0400

    add controlled inputs

--- src/pages/NewCar/NewCar.jsx
+++ src/pages/NewCar/NewCar.jsx
@@ -1,26 +1,34 @@
 import { useState } from "react"
 import "./NewCar.css"
-import {v4 as uuidv4} from 'uuid'
+import { v4 as uuidv4 } from 'uuid'
 
 const NewCar = (props) => {
 
   const [formData, setFormData] = useState({
-    _id: uuidv4().replaceAll('-',''),
+    _id: uuidv4().replaceAll('-', ''),
     make: '',
     model: '',
     year: '',
     color: '',
   })
 
+  function handleChange(evt) {
+    setFormData({ ...formData, [evt.target.name]: evt.target.value })
+  }
+
   return (
     <>
       <div className="new-car-container">
         <h1>Add Car</h1>
         <form>
-          <input type="text" name="make" placeholder="Make" />
-          <input type="text" name="mode" placeholder="Model" />
-          <input type="text" name="year" placeholder="Year" />
-          <input type="text" name="color" placeholder="Color" />
+          <input onChange={handleChange} value={formData.make} type="text" name="make" placeholder="Make" />
+
+          <input onChange={handleChange} value={formData.model} type="text" name="model" placeholder="Model" />
+
+          <input onChange={handleChange} value={formData.year} type="text" name="year" placeholder="Year" />
+
+          <input onChange={handleChange} value={formData.color} type="text" name="color" placeholder="Color" />
+
           <button type="submit">Add Car 🚘</button>
         </form>
       </div>
